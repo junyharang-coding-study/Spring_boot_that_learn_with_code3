ffa2b24096a9ddd6bc58d8fcca9285849e24eb72
junyharang <junyharang8592@gmail.com>
2021-11-01 20:44:06 +0900

    211101 : basic setting first commit

---
+++ src/test/java/org/junyharang/boardstudy/BoardStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.junyharang.boardstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
